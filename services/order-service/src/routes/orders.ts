9a9a6b0615b8acf8f225c366d89d4f81e98d91b2
manimit10 <m.tahriri.z@gmail.com>
2024-07-23 23:18:38 +0200

    new project strated!

--- services/order-service/src/routes/orders.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllOrders, createOrder } from '../controllers/orders';
-
-const router = Router();
-
-router.get('/', getAllOrders);
-router.post('/', createOrder);
-
-export default router;
